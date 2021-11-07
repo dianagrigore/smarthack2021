e09c39bc64ea8422e33a7292af55d4e2e0f89deb
Stefannsen <stefan.negulescu@my.fmi.unibuc.ro>
2021-11-07 11:12:57 +0200

    added passGen

--- src/main/java/com/example/keystorage/service/KeyService.java
+++ src/main/java/com/example/keystorage/service/KeyService.java
@@ -1,4 +1,24 @@
 package com.example.keystorage.service;
 
+
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+
 public class KeyService {
+    public String generateRSA() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(1024);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        RSAPublicKeySpec rsaPublicKeySpec = keyFactory.getKeySpec(publicKey, RSAPublicKeySpec.class);
+        RSAPrivateKeySpec rsaPrivateKeySpec = keyFactory.getKeySpec(privateKey, RSAPrivateKeySpec.class);
+
+        return "";
+    }
 }
--- src/main/java/com/example/keystorage/service/PasswordService.java
+++ src/main/java/com/example/keystorage/service/PasswordService.java
@@ -1,4 +1,43 @@
 package com.example.keystorage.service;
 
+
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.PasswordGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class PasswordService {
+
+    public String generatePassword(int length, boolean alpha, boolean upper, boolean lower, boolean special, boolean num){
+        CharacterRule digits = new CharacterRule(EnglishCharacterData.Digit);
+        CharacterRule rule_1 = new CharacterRule(EnglishCharacterData.Alphabetical);
+        CharacterRule rule_2 = new CharacterRule(EnglishCharacterData.UpperCase);
+        CharacterRule rule_3 = new CharacterRule(EnglishCharacterData.LowerCase);
+        CharacterRule rule_4 = new CharacterRule(EnglishCharacterData.Special);
+
+        List<CharacterRule> rulesList = new ArrayList<>();
+        if(alpha){
+            rulesList.add(rule_1);
+        }
+        if(upper){
+            rulesList.add(rule_2);
+        }
+        if(lower){
+            rulesList.add(rule_3);
+        }
+        if(special){
+            rulesList.add(rule_4);
+        }
+        if(num){
+            rulesList.add(digits);
+        }
+
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        String password = passwordGenerator.generatePassword(length, rulesList);
+
+        return password;
+    }
 }
